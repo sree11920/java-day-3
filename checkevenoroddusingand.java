c3140603adc613b2e46bc9762e87a893b21a186c
sree11920 <kasireddygarihemasree@gmail.com>
2024-12-04 10:26:40 +0530

    Add files via upload

---
+++ checkevenoroddusingand.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class checkevenoroddusingand{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if((a&1)==0)
+        System.out.println("even");
+        else
+        System.out.println("odd");
+    }
+}
