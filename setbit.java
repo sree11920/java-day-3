ef8a85a894056506266884887024c50a31f37f8d
sree11920 <kasireddygarihemasree@gmail.com>
2024-12-04 10:58:56 +0530

    Add files via upload

---
+++ setbit.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class setbit{
+    public static void main(String args[]){
+        int a=10;
+        int pos=1;
+        if((a&(1<<pos))!=0){
+            System.out.println("set bit");
+        }else{
+            System.out.println("not a set bit");
+        }
+    }
+}
